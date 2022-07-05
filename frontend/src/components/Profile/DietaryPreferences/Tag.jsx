df0e0a76c9fece7bcb8760f811629274f5130221
Adelle Vo <amv@fb.com>
2022-07-05 13:34:09 -0700

    added patch request in frontend for array removal

--- frontend/src/components/Profile/DietaryPreferences/Tag.jsx
+++ frontend/src/components/Profile/DietaryPreferences/Tag.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import deleteButton from "../../Shared/assets/DeleteButton.svg";
+import API from "../../../utils/API";
 
 export default function Tag(props) {
   const [showTag, setShowTag] = useState(true);
+
+  // ---- Modify user's dietary profile ----
+
+  const deleteFromLikes = async () => {
+    try {
+      // remove food from likes array and reset state
+      // props.setData([...props.data, newItem]);
+      // setNewItem("");
+      setShowTag(false);
+      // update in database
+      const config = { headers: { "Content-Type": "application/json" } };
+      const body = { food: props.text };
+      const res = await API.patch("api/v1/auth/dietaryProfile/delete", body, config);
+      console.log("res: ", res);
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }
+  };
 
   return (
     <div>
@@ -14,7 +34,8 @@ export default function Tag(props) {
               className="tag-img"
               src={deleteButton}
               alt="delete button"
-              onClick={() => setShowTag(false)}
+              // onClick={() => setShowTag(false)}
+              onClick={deleteFromLikes}
             />
           )}
         </div>
