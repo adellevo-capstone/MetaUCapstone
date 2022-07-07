d1b3f0257765bd03c375b0826a56bc29b56b1cc0
Adelle Vo <amv@fb.com>
2022-07-07 10:23:59 -0700

    implemented search bar to find users in database

---
+++ frontend/src/components/Groups/GroupSearch.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from "axios";
+import API from "../../utils/API";
+
+export default function GroupSearch(props) {
+  //   const [restaurant, setRestaurant] = useState("");
+  const [usersToAdd, setUsersToAdd] = useState([]);
+  const [displayedUsers, setDisplayedUsers] = useState([]);
+
+  const findUsers = async (restaurantToAdd) => {
+    try {
+      const filteredUsers = props.allUsers.filter(
+        (user) =>
+          user.firstName.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
+          user.lastName.toLowerCase().includes(props.searchQuery.toLowerCase())
+      );
+      setDisplayedUsers(filteredUsers);
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }
+  };
+
+  const addUsers = async (restaurantToAdd) => {
+    try {
+      const config = { headers: { "Content-Type": "application/json" } };
+      const body = { restaurantToAdd: restaurantToAdd };
+      await API.patch("api/v1/auth/allUsers", body, config);
+      // await API.patch("api/v1/auth/dietaryProfile/delete", body, config);
+      props.loadFavoriteRestaurants();
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="search-popup">
+      <h1>Search for a user to add</h1>
+      <div className="search-popup-content">
+        <input
+          className="search"
+          type="text"
+          name="user"
+          placeholder="Type in a name..."
+          onChange={(e) => props.setSearchQuery(e.target.value)}
+        />
+        <button onClick={findUsers}>Search</button>
+        <div>
+          <h2>Search results:</h2>
+          <ul>
+            {displayedUsers.map((user, index) => (
+              <li
+                key={index}
+                onClick={() =>
+                  setUsersToAdd([
+                    ...usersToAdd,
+                    { name: user.firstName + " " + user.lastName, userId: user._id },
+                  ])
+                }
+              >
+                {user.firstName} {user.lastName}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2>Users to add:</h2>
+          <ul>
+            {usersToAdd.map((user, index) => (
+              <li key={index}>{user.name}</li>
+            ))}
+          </ul>
+          <button onClick={() => addUsers}>Confirm</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/Groups/Groups.jsx
+++ frontend/src/components/Groups/Groups.jsx
@@ -1,36 +1,65 @@
 import React, { useState, useEffect } from "react";
 import "../Groups/Groups.css";
 import API from "../../utils/API";
+import Popup from "reactjs-popup";
+import GroupSearch from "./GroupSearch";
 
 export default function Groups(props) {
   // const members = ["Adelle Vo", "Naomi Donato", "Janice Park"];
   const [members, setMembers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [favorites, setFavorites] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
 
   useEffect(() => {
-    loadUsers();
+    loadAllUsers();
   }, []);
 
-  const loadUsers = async () => {
+  const loadAllUsers = async () => {
     try {
       const res = await API.get("api/v1/auth/allUsers");
       console.log(res.data);
-      setMembers(res.data);
+      setAllUsers(res.data);
     } catch (err) {
       console.log(err.response);
     }
   };
 
   return (
-    <div className="groups">
-      <div>
-        <h2>305 group</h2>
-        {members.map((member) => (
-          <div>
-            {member.firstName} {member.lastName}
-          </div>
-        ))}
-        {/* popup for all members in database */}
-        <button>Add a member</button>
+    <div>
+      <Popup
+        closeOnDocumentClick
+        modal
+        nested
+        trigger={<button> Create a group </button>}
+        style={{
+          minWidth: "40em",
+        }}
+      >
+        <GroupSearch
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          location={location}
+          setLocation={setLocation}
+          loadAllUsers={loadAllUsers}
+          allUsers={allUsers}
+          // members={members}
+          // setNewRestaurant={setNewRestaurant}
+        />
+      </Popup>
+      <h2>My groups</h2>
+      <div className="groups">
+        {/* <div>
+          <h2>305 group</h2>
+          {members.map((member) => (
+            <div>
+              {member.firstName} {member.lastName}
+            </div>
+          ))}
+         
+          <button>Add a member</button>
+        </div> */}
       </div>
     </div>
   );
